816dde73c349badbaf66f6336cee2695b469d2b5
xwnn <xwnn@github.com>
2020-08-19 18:12:57 -0500

    Updates

---
+++ precache-manifest.5ce3f16e2130c488ec71437d61102286.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7d88b27c7401fd5a9f062f14aed17b",
+    "url": "/portfolioHome/index.html"
+  },
+  {
+    "revision": "cf4cf86c9c4c0deec353",
+    "url": "/portfolioHome/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdfc518e8c886d08cf43",
+    "url": "/portfolioHome/static/js/2.5f4523cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolioHome/static/js/2.5f4523cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf4cf86c9c4c0deec353",
+    "url": "/portfolioHome/static/js/main.2fa0e6e1.chunk.js"
+  },
+  {
+    "revision": "907d1632417c32b5dc2b",
+    "url": "/portfolioHome/static/js/runtime-main.207bd8df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolioHome/static/media/logo.5d5d9eef.svg"
+  }
+]);
